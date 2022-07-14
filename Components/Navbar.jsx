2ac1b73824ae453e7278fb7dfbe7b6d0cb49f69e
Abhi-4793 <abhinavkrishnapy2@gmail.com>
2022-07-15 00:25:30 +0530

    Logo change and add toml file

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -48,8 +48,8 @@ const Navbar = () => {
         <Link href="/">
           <Image
             src="/../public/assets/logo.png"
-            width="100"
-            height="100"
+            width="120"
+            height="120"
             alt="/"
           />
         </Link>
