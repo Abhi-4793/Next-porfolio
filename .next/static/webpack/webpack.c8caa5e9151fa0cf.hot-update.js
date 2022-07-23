fa03d204ad4968da4a2df26147fb74397efe8585
Abhi-4793 <abhinavkrishnapy2@gmail.com>
2022-07-23 21:03:15 +0530

    config tailwind

--- .next/static/webpack/webpack.c8caa5e9151fa0cf.hot-update.js
+++ .next/static/webpack/webpack.c8caa5e9151fa0cf.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "e6d0f940b3b5782f"; }
+/******/ 	__webpack_require__.h = function() { return "e0dc7768361dd9bf"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
